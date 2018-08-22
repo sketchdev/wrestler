fa608137780d8137ae9fcaff21b610375509131e
Cody Frederick <cody@sketchdev.io>
2018-08-22 16:00:09 -0500

    starting to refactor some testing

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,34 @@
 require('dotenv').config({ path: '.env.test' });
 
-const db = require('../lib/db');
+let db;
 
 const initDatabase = async () => {
-  global.testDb = await db.connect();
+  const d = await require('../lib/db').connect();
+  global.testDb = d;
+  db = d;
 };
 
 (async () => {
   await initDatabase();
 })().then(run);
+
+
+
+
+const express = require('express');
+const wrestler = require('../wrestler');
+const supertest = require('supertest');
+const nodemailer = require('nodemailer');
+const uuid = require('uuid/v4');
+
+exports.buildWrestler = (wrestlerOptions) => {
+  const transport = { name: 'wrestler', version: '1', send: (mail, callback) => callback(null, { envelope: {}, messageId: uuid() }) };
+  const transporter = nodemailer.createTransport(transport);
+
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(wrestler(Object.assign({}, wrestlerOptions, { db, email: { transporter } })));
+
+  return supertest(app);
+};
